35412f7139fcdf1ce65a123962118b29b9f37682
ztionbar <ztion1990@gmail.com>
2025-02-02 23:47:35 +0800

    新增gh-pages套件、axios套件、調整vite.config.js 檔案內容

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 開發中、產品路徑
+  base: process.env.NODE_ENV === 'production' ? '/vite-react-sample/' : '/',
   plugins: [react()],
 })
