e9e36559276d38af9cfa35a0b35e4b814b11c496
ztionbar <ztion1990@gmail.com>
2025-02-04 23:07:18 +0800

    新增bootstrap modal 客製化操作、新增 all.scss檔案

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,64 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import axios from 'axios'
+import { Modal } from 'bootstrap'
+
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const modalRef = useRef(null)
+  const customModal = useRef(null)
+    useEffect(() => {
+      (async () =>{
+        const res = await axios.get('https://randomuser.me/api')
+        console.log(res);
+        openModal();
 
+        setTimeout(() =>{
+          closeModal()
+        }, 2000);
+      })()
+    },[])
+
+    useEffect(() =>{
+      // console.log(modalRef.current);
+      customModal.current = new Modal(modalRef.current);
+      // customModal.current.show()
+    }, [])
+
+    const openModal = () =>{
+       customModal.current.show()
+    }
+    const closeModal = () =>{
+      customModal.current.hide()
+   }
+    
   return (
     <>
+      < button type="button" className="btn btn-primary" onClick={() => openModal()}>
+        Launch demo modal
+      </button>
+
+      <div className="modal fade" ref={modalRef} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+                <p className="text-start">視窗彈出了</p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,7 +69,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className='btn btn-primary' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import 'bootstrap'
+import './assets/all.scss'
+// import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
