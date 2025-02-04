a0e302c6a0c60091cd83adb19291925ebbd005f1
ztionbar <ztion1990@gmail.com>
2025-02-04 20:58:28 +0800

    修改vite.config.js 檔案內容

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  // 開發中、產品路徑
-  base: process.env.NODE_ENV === 'production' ? '/vite-react-sample/' : '/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/vite-react-sample/' : '/',
   plugins: [react()],
-})
+}))
